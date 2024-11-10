7c7cf27ccff4d9bf889fae6ef2c797ac7206c3b9
Yuhi-Sato <91863685+Yuhi-Sato@users.noreply.github.com>
2024-11-10 19:46:57 +0900

    dbのコネクション

--- go/main.go
+++ go/main.go
@@ -98,7 +98,8 @@ func connectDB(logger echo.Logger) (*sqlx.DB, error) {
 	if err != nil {
 		return nil, err
 	}
-	db.SetMaxOpenConns(10)
+	db.SetMaxOpenConns(150)
+	db.SetMaxIdleConns(150)
 
 	if err := db.Ping(); err != nil {
 		return nil, err
